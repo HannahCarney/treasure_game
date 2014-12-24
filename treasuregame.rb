7b6cd191ec3266e53e991e5c5b8b944ac42e8b55
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-24 16:39:40 +0000

    can now change the description of treasures when damaged

--- treasuregame.rb
+++ treasuregame.rb
@@ -11,8 +11,8 @@ class Thing
     @@num_things +=1 #increment @@num_things
   end
 
-  def to_s #override default to_s methog
-    return "(Thing.to_s):: The #{@name} Thing is 3{@description}"
+  def to_s #override default to_s method
+    return "(Thing.to_s):: The #{@name} Thing is #{@description}"
   end
 
   def show_classvars
@@ -52,5 +52,18 @@ t2 = Treasure.new("Dragon Horde", "a huge pile of jeweles", 550)
 room1 = Room.new("Crystal Grotto", "A glittery cavern")
 room2 = Room.new("Dark Cace", "A gloomy hole in the rocks")
 room3 = Room.new("Forest Glade", "A verdant clearing filled with shimmering light")
-
+#intiliazes a map with the rooms just created
 mymap = Map.new([room1, room2, room3])
+
+puts "This is the treasure1: #{t1.inspect}"
+puts "This is the treasure2: #{t2.inspect}"
+
+puts "Treasure2 is #{t2.to_s}"
+
+puts "t1 name = #{t1.name}, description = #{t1.description}, value = #{t1.value}"
+# change the description of the sword to be tarnished
+t1.description << " now somewhat tarnished"
+puts "Treasure1 is #{t1.description}"
+
+puts "room1 name=#{room1.name}, description=#{room1.description}" 
+puts ( t1.show_classvars )
